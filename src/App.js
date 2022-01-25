a844ff83df9ca26088acdbe8b8739ae1b631ae0a
jessica <fuerte.jessica10@gmail.com>
2022-01-25 15:55:12 -0600

    did app js routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
-  );
+
+const FriendsList = ()=>{
+  return (<h2>FriendsList</h2>)
+}
+
+const AddFriend = ()=>{
+  return (<h2>Add Friend</h2>)
+}
+
+const Login = ()=>{
+  return(<h2>Login</h2>)
+}
+
+function App () {
+  return(
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/login">
+          <Redirect to="/" />
+        </Route>
+        <Route exact path="/friends">
+          <FriendsList />
+        </Route>
+        <Route exact path="/friends/add">
+          <AddFriend />
+        </Route>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
