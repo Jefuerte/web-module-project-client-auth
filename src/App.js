84b0d86488b7eb70ff555e3f211648fe28b45759
jessica <fuerte.jessica10@gmail.com>
2022-01-25 16:20:12 -0600

    added component files

--- src/App.js
+++ src/App.js
@@ -12,7 +12,20 @@ const AddFriend = ()=>{
 }
 
 const Login = ()=>{
-  return(<h2>Login</h2>)
+  return(<div>
+    <h1>Login</h1>
+    <form>
+      <div>
+      <label htmlFor="username">Username:</label>
+      <input id="username"/>
+      </div>
+      <div>
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password"/>
+      </div>
+      <button>Submit</button>
+    </form>
+      </div>)
 }
 
 function App () {
